762fdf059029207421fa237bfd328c7c9321306a
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-01-29 13:44:05 -0500

    lakjg

---
+++ javascript/statements.js
@@ -0,0 +1,25 @@
+//Statement syntax is JavaScript
+
+//Constants
+const pi = 3.141592654;
+
+//printing
+console.log(pi);
+
+//For loop
+for (let x = 0; x < 3; x++){
+  console.log(x);
+}
+
+//While loop
+let x = 0;
+while (x < 3) {
+  console.log(x);
+  x += 1;
+}
+
+if (x === 3){
+  console.log('x is now 3');
+} else {
+  console.log(`x is now ${x}`);
+}
