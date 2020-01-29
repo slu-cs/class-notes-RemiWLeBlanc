78f880e4e0a84faa8d76bec7fd4507315972a5c5
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-01-29 13:37:06 -0500

    javascipt syntax

---
+++ javascript/expressions.js
@@ -0,0 +1,55 @@
+// Expression syntax in JavaScript
+
+//Booleans
+true
+false
+
+//Logic
+!false
+true && false
+true || false
+
+//Numbers (all one type)
+3.14
+
+//Equality
+3 === 3.0 //t
+3 !== 3.14
+
+//Ordering
+3 > 2
+3 >= 2
+2 < 3
+2 <= 3
+
+//Strings
+'hello'
+"hello"
+'a' + 'b' === 'ab' //t
+
+//Arithemtic
+3 + 2 === 5
+3 - 2 === 1
+3 * 2 === 6
+3 / 2 === 1.5
+3 & 2 === 1
+3 ** 2 === 9
+
+//Type coercion
+'3' + 2 === '32'
+'3' + true === '3true'
+'3' - 2 === 1
+3 + true === 4
+
+//loose Equality
+'3' == 3
+true == 1
+false == 0
+false == ''
+'' == 0
+
+// template Strings
+'3+2 is ' + 3+2 // '3+2 is 32'
+`3+2 is ${3+2}` //need to use accent ticks
+
+ 
