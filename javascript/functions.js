20f5699bc1c9dd09d5bcb884e52fa62d65f9ac81
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-03 13:33:53 -0500

    functions and methods and constructors and objects

--- javascript/functions.js
+++ javascript/functions.js
@@ -18,3 +18,37 @@ const result = print(7,8);
 console.log(result); //undefined
 print(9); //9, undefined
 print();//undefined, undefined
+
+
+//attach functions to objects so they become methods
+const rectangle = {
+  widt: 10,
+  height: 20,
+  area: function(){
+    return this.width * this.height;
+  }
+};
+
+//method call
+console.log(rectangle.area());
+
+//constructor defintion
+const Rectangle = function(width, height){
+  this.width = width;
+  this.height = height;
+};
+
+//shared method
+Rectangle.prototype.area = function(){
+  return this.width * this.height;
+} //all Rectangle objects will have area functions
+
+
+//constructing objects
+const small = new Rectangle(1,2);
+const large = new Rectangle(10,20);
+
+
+//method calls
+console.log(small.area());
+console.log(large.area());
