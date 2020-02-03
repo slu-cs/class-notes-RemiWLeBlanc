61583878e3c6ea6cb3ab95eab3f3700761d10c27
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-03 14:01:08 -0500

    exercise 2

--- javascript/objectexercises.js
+++ javascript/objectexercises.js
@@ -20,3 +20,27 @@ for(const course of courses){
   }
 }
 console.log(max);
+
+
+//2. Define a constructor function for Book objects. Each book should have a title
+//and an array of authors. Define a shared method for books that lets you check whether
+//a given author was one of the authors in this book.
+
+const Book = function(title, authors){
+  this.title = title,
+  this.authors = authors
+};
+Book.prototype.authorCheck = function(author){
+  for(const a of this.authors){
+    if(a === author){
+      return true;
+    }
+  }
+  return false;
+}
+
+const b1 = new Book('B1', ['Remi LeBlanc', 'Isaac Brinkman']);
+const b2 = new Book('B2'. ['Remi LeBlanc', 'Wazzzaz']);
+
+console.log(b1.authorCheck('Remi LeBlanc'));
+console.log(b1.authorCheck('Wazzza'));
