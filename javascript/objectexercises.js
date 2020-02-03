62acaef03f7e3a2ea5f16a05fea77c5d8d114d99
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-03 13:54:41 -0500

    object error fix

--- javascript/objectexercises.js
+++ javascript/objectexercises.js
@@ -3,7 +3,10 @@
 //1. Create an array of objects representing the courses you are taking this semester.
 //Each course should have a subject (e.g. 'CS') and a number (e.g. 332).
 //Iterate through this array to find the largest course number in your schedule.
-const Course = function(class, number);
+const Course = function(course, number){
+  this.course = course;
+  this.number = number;
+};
 const pl = new Course('CS', 364);
 const web = new Course('CS', 332);
 const phys = new Course('MUS', 120);
