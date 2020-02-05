5f4fc6ececada11aa5d283a88b93d587599001fd
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-05 14:20:00 -0500

    asdg

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -1,6 +1,6 @@
-const numbers = [1, 2, 3, 4, 5];
-const odds = numbers.filter(x => x%2 > 0);
-console.log(odds);
+// const numbers = [1, 2, 3, 4, 5];
+// const odds = numbers.filter(x => x%2 > 0);
+// console.log(odds);
 
 const filter = function(nums, f){
   const remi = [];
@@ -12,6 +12,6 @@ const filter = function(nums, f){
   return remi;
 };
 
-const numbers = [1, 2, 3, 4, 5];
-const odds = filter(numbers, x => x%2 > 0);
+const numbers1 = [1, 2, 3, 4, 5];
+const odds = filter(numbers1, x => x%2 > 0);
 console.log(odds);
