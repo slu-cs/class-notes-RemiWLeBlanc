a527facda36da518285048735f83f8b98f10624d
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-05 14:17:10 -0500

    exercise 1

---
+++ javascript/callbackexercises.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3, 4, 5];
+const odds = numbers.filter(x => x%2 > 0);
+console.log(odds);
+
+const filter = function(nums, f({
+  new = [];
+  for(const n of nums){
+    if(f() == 1){
+      new.push(n)
+    }
+  }
+});
+
+const numbers = [1, 2, 3, 4, 5];
+const odds = filter(numbers, x => x%2 > 0);
+console.log(odds);
