beca9f01c11c4f66bf200c331051336044e6d0e7
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-02-05 14:20:58 -0500

    exercsie 1 complete

--- javascript/callbackexercises.js
+++ javascript/callbackexercises.js
@@ -2,6 +2,7 @@
 // const odds = numbers.filter(x => x%2 > 0);
 // console.log(odds);
 
+//Question 1
 const filter = function(nums, f){
   const remi = [];
   for(const n of nums){
@@ -15,3 +16,5 @@ const filter = function(nums, f){
 const numbers1 = [1, 2, 3, 4, 5];
 const odds = filter(numbers1, x => x%2 > 0);
 console.log(odds);
+
+//Question 2
