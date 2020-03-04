2865974397397861fbed872ff63f10d4e29bbe03
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-03-04 13:35:53 -0500

    other urls

--- express/hello/app.js
+++ express/hello/app.js
@@ -6,9 +6,32 @@ const express = require('express');
 const app = express();
 
 //respond to a single request
+
+// http://cs-linuxlab-27.stlawu.edu:3000/
 app.get('/', function(request, response){
   response.send('Hello Remi')
-})
+});
+
+// cs-linuxlab-27.stlawu.edu:3000/foo
+app.get('/foo', function(request, response){
+  response.send('Hello foo')
+});
+
+// cs-linuxlab-27.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response){
+  response.send('Hello bar')
+});
+
+// cs-linuxlab-27.stlawu.edu:3000/foo/zap/z (for any z)
+app.get('/zap/:z', function(request, response){
+  response.send(`Hello ${request.params.z}`)
+});
+
+// cs-linuxlab-27.stlawu.edu:3000/foo/zap?x=X&y=Y
+app.get('/zap', function(request, response){
+  response.send(`Hello zap with ${request.query.x}` amd ${request.query.y})
+});
+
 
 //start the server
 app.listen(3000);
