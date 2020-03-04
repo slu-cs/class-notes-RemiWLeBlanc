224d5ef2bcf79e20ced24487a116ef7cc8ed529d
Remi LeBlanc <rwlebl16@stlawu.edu>
2020-03-04 13:58:47 -0500

    ljhgul

--- express/bakery/app.js
+++ express/bakery/app.js
@@ -1,39 +1,40 @@
-// hello world web server
-
+// Hello world web server
 const express = require('express');
 
-//Create the server
+// Create the server
 const app = express();
 
-// logs requests to the console
-app.use(function(request, response, next){
-  console.log('----------------------------', new Date().toLocaleTimeString());
+// Log requests to the console
+app.use(function(request, response, next) {
+  console.log('---------------------', new Date().toLocaleTimeString());
   console.log(request.method, request.url);
   console.log('Body =', request.body);
+  next(); // Keep handling this request
 });
 
-app.get('/',function(request, response){
+// Home page
+app.get('/', function(request, response) {
   response.send(`
     <h1>Bakery</h1>
     <ul>
       <li><a href="/cakes">Cakes</a></li>
       <li><a href="/pies">Pies</a></li>
     </ul>
-    `);
+  `);
 });
 
-//handle undefined routes
-app.use(function(request, response, next){
+// Handle undefined routes
+app.use(function(request, response, next) {
   console.log('Replied with 404');
   response.status(404).end();
 });
 
-// handle other errors
-app.use(function(error, request, response, next){
+// Handle other errors
+app.use(function(error, request, response, next) {
   console.error(error.stack);
-  response.status(500).send(error.message); // dont show what is going wrong in a real server
+  response.status(500).send(error.message);
 });
 
-//start the server
+// Start the server
 app.listen(3000);
-console.log("Server is ready!")
+console.log('Server is ready.');
